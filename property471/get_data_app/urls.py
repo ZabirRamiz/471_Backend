75b9a78a7b739f8dfea25baabc2c920ca91c659e
Zabir Ramiz <zabir.ramiz@g.bracu.ac.bd>
2023-11-18 07:05:50 +0600

    added  get_only_user/agent_data

--- property471/get_data_app/urls.py
+++ property471/get_data_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('user_data',views.user_data),
     path('employee_data',views.employee_data),
     path('property_data',views.property_data),
+    path('only_user_data',views.only_user_data),
+    path('only_agent_data',views.only_agent_data),
+    path('only_support_data',views.only_support_data),
+    path('user_property', views.user_property)
 ]
