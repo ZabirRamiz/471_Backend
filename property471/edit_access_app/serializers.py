8ab0e0462f38216455da64b46afb6b0dd6724aa6
Nafisa7503 <145420919+Nafisa7503@users.noreply.github.com>
2023-12-01 19:27:03 +0600

    Employee lost and found
    merged with frontend

--- property471/edit_access_app/serializers.py
+++ property471/edit_access_app/serializers.py
@@ -1,3 +1,3 @@
 from rest_framework import serializers
 from .models import *
-from signup_login_app.serializers import userSerializer
+from signup_login_app.serializers import userSerializer,employeeSerializer
