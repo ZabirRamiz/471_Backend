d64af272ed80293a3318a4aa402ef35c1c84738a
Zabir Ramiz <zabir.ramiz@g.bracu.ac.bd>
2023-12-08 16:29:41 +0600

    del comments in payment view

--- property471/payment_app/views.py
+++ property471/payment_app/views.py
@@ -90,61 +90,6 @@ def ask_approval(request):
     return JsonResponse({"message": "insufficient funds"}, status=202)
 
 
-# @api_view(["POST"])
-# def give_approval(request):
-#     buyer_id = request.data["buyer_id"]
-#     property_id = request.data["property_id"]
-
-#     property_val = property.objects.get(property_id=property_id)
-
-#     seller_id = property_val.user_id_id
-#     print("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj")
-#     print(seller_id)
-#     agent_id = property_val.agent_id_id
-#     print(agent_id)
-
-
-#     seller_val = user.objects.get(user_id=seller_id)
-#     buyer_val = user.objects.get(user_id=buyer_id)
-#     agent_val = employee.objects.get(employee_id=agent_id)
-
-#     property_val.admin_approval = True
-#     property_val.user_id_id = buyer_id
-#     property_val.market_status = "Available For Sale"
-#     property_val.agent_id_id = "NULL"
-#     property_val.support_id_id = "NULL"
-
-#     buyer_val.wallet = int(buyer_val.wallet) - int(property_val.property_price)
-#     agent_commission = int(property_val.property_price) * float(
-#         f".0{agent_val.commission}"
-#     )
-#     agent_val.wallet = int(agent_val.wallet) + agent_commission
-#     seller_val = (
-#         int(seller_val.wallet) + int(property_val.property_price) - agent_commission
-#     )
-
-#     user.save(seller_val)
-#     user.save(buyer_val)
-#     employee.save(agent_val)
-#     property.save(property_val)
-
-#     seller_Serializer = userSerializer(seller_val)
-#     buyer_Serializer = userSerializer(buyer_val)
-#     agent_Serializer = employeeSerializer(agent_val)
-#     property_Serializer = propertySerializer(property_val)
-
-#     return JsonResponse(
-#         {
-#             "message": "set admin approval and handled the payments",
-#             "property_data": property_Serializer.data,
-#             "seller_data": seller_Serializer.data,
-#             "buyer_data": buyer_Serializer.data,
-#             "agent_data": agent_Serializer.data,
-#         },
-#         status=201,
-#     )
-
-
 @api_view(["POST"])
 def give_approval(request):
     buyer_id = request.data["buyer_id"]
